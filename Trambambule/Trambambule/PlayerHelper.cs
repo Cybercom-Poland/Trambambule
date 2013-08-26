f885a481a0ca32d189dc9d1b554a3da2d00e807d
Maciej Górski <maciek.gorski@gmail.com>
2013-08-26 16:39:09 +0200

    new player rating calculations stub

--- Trambambule/Trambambule/PlayerHelper.cs
+++ Trambambule/Trambambule/PlayerHelper.cs
@@ -23,8 +23,68 @@ namespace Trambambule
             TeamMatchPlayer playerB2LastMatchData,
             int goalsA, int goalsB)
         {
-            
+            if (playerA1LastMatchData == null)
+            {
+                playerA1LastMatchData = CreateEmptyMatchData();
+            }
+            if (playerA2LastMatchData == null)
+            {
+                playerA2LastMatchData = CreateEmptyMatchData();
+            }
+            if (playerB1LastMatchData == null)
+            {
+                playerB1LastMatchData = CreateEmptyMatchData();
+            }
+            if (playerB2LastMatchData == null)
+            {
+                playerB2LastMatchData = CreateEmptyMatchData();
+            }
+            playerA1LastMatchData.RD = CalculateCurrentRD(playerA1LastMatchData);
+            playerA2LastMatchData.RD = CalculateCurrentRD(playerA2LastMatchData);
+            playerB1LastMatchData.RD = CalculateCurrentRD(playerB1LastMatchData);
+            playerB2LastMatchData.RD = CalculateCurrentRD(playerB2LastMatchData);
+            FillSinglePlayerRating(ref playerA1MatchData, playerA1LastMatchData, playerA2LastMatchData, playerB1LastMatchData, playerB2LastMatchData, goalsA, goalsB);
+            FillSinglePlayerRating(ref playerA2MatchData, playerA2LastMatchData, playerA1LastMatchData, playerB1LastMatchData, playerB2LastMatchData, goalsA, goalsB);
+            FillSinglePlayerRating(ref playerB1MatchData, playerB1LastMatchData, playerB2LastMatchData, playerA1LastMatchData, playerA2LastMatchData, goalsB, goalsA);
+            FillSinglePlayerRating(ref playerB2MatchData, playerB2LastMatchData, playerB1LastMatchData, playerA1LastMatchData, playerA2LastMatchData, goalsB, goalsA);
             return true;
+        }
+
+        private static TeamMatchPlayer CreateEmptyMatchData()
+        {
+            TeamMatchPlayer matchData = new TeamMatchPlayer();
+            matchData.Rating = 1500;
+            matchData.RD = 350;
+            matchData.Timestamp = DateTime.Now;
+            return matchData;
+        }
+
+        private static void FillSinglePlayerRating(
+            ref TeamMatchPlayer newMatchData,
+            TeamMatchPlayer oldMatchData,
+            TeamMatchPlayer allyMatchData,
+            TeamMatchPlayer firstOppMatchData,
+            TeamMatchPlayer secondOppMatchData,
+            int ourGoals, int oppGoals)
+        {
+            double result = CalculateResult(ourGoals, oppGoals);
+        }
+
+        private static double CalculateResult(int ourGoals, int oppGoals)
+        {
+            if (ourGoals == 0 && oppGoals == 0)
+            {
+                return 0.5;
+            }
+            else
+            {
+                return ((double)ourGoals) / (ourGoals + oppGoals);
+            }
+        }
+
+        public static double CalculateCurrentRD(TeamMatchPlayer lastMatchData)
+        {
+            return 350;
         }
     }
 }
